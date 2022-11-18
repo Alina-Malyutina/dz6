3fd715ff2a0f1f81c8ef41941791adfe938cf255
Alina Malyutina <linamalutina1@gmail.com>
2022-11-18 11:56:24 +0300

    task 42 - count positive

---
+++ task42/Program.cs
@@ -0,0 +1,27 @@
+﻿int countPositive (int [] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+Console.WriteLine("This program assepts your numbers and count quantity of positive numbers.");
+Console.Write("Enter quantity of numbers, that you want to enter: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int [] arrayNumbers = new int [N];
+
+for (int i = 0; i < arrayNumbers.Length; i++)
+{
+    Console.Write("Enter number: ");
+    arrayNumbers[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int res = countPositive(arrayNumbers);
+Console.WriteLine($"Here is {res} positive numbers");
